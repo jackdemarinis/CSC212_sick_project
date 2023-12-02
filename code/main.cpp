ebaabd7497a465075d526c2ceb959c950f8f2a0b
zacknotarianni <123593640+zacknotarianni@users.noreply.github.com>
2023-12-02 16:13:27 -0500

    Update main.cpp

--- code/main.cpp
+++ code/main.cpp
@@ -1,1 +1,11 @@
+#include <iostream>
+#include <vector>
+#include "convexHull.h"
 
+
+int main(int argv, char* argc[]) {
+
+    std::string file = argc[1];
+    convexHull hullObj; // Create an object of the convexHull class
+    std::vector<Point> data = hullObj.createConvexHull(file);
+}
