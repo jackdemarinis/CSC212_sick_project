1b3f00cf30b7a366a21f1b2ce4fdadd581721b7a
zacknotarianni <123593640+zacknotarianni@users.noreply.github.com>
2023-12-03 20:49:03 -0500

    Update convexHull.cpp

--- code/convexHull.cpp
+++ code/convexHull.cpp
@@ -4,6 +4,8 @@
 #include <sstream>
 #include <utility>
 #include <vector>
+#include <iomanip>
+#include <string>
 
 void convexHull::detectShape(std::vector<Point> convexHull)
 {
@@ -57,12 +59,12 @@ void convexHull::detectShape(std::vector<Point> convexHull)
     }
 }
 
-int convexHull::findDistance(Point p1, Point p2)
+int convexHull::findDistance(Point p1, Point p2) //calcualte distance between 2 points (using a^2 + b ^2 = c^2)
 {
     return sqrt(pow(p1.x-p2.x,2) + pow(p1.y-p2.y,2));
 }
 
-std::vector<Point> convexHull::createConvexHull(std::string filename)
+std::vector<Point> convexHull::createConvexHull(std::string filename) //main function to create the CH
 {
     std::vector<Point> dataSet;
     std::vector<Point> convexHull;
@@ -90,12 +92,11 @@ std::vector<Point> convexHull::createConvexHull(std::string filename)
     convexHull.push_back(dataSet[0]);
     convexHull.push_back(dataSet[1]);
 
-    for(int i = 2; i < dataSet.size(); i++) //build convex hull
-    {
-        if(orientation(convexHull[convexHull.size()-2],convexHull[convexHull.size()-1],dataSet[i]) == 1) //if counterclockwise
-        {
-            convexHull.push_back(dataSet[i]); //add to convexhull
+    for(int i = 2; i < dataSet.size(); i++) {
+        while(convexHull.size() > 1 && orientation(convexHull[convexHull.size()-2], convexHull[convexHull.size()-1], dataSet[i]) != 1) {
+            convexHull.pop_back(); // Remove points creating a clockwise or collinear turn
         }
+        convexHull.push_back(dataSet[i]); // Add the point creating a counterclockwise turn
     }
     print(convexHull);
     std::cout<<std::endl;
@@ -103,36 +104,104 @@ std::vector<Point> convexHull::createConvexHull(std::string filename)
     return convexHull;
 }
 
-void convexHull::saveToDotFile(const std::vector<Point>& points, const std::string& filename) {
-    std::ofstream dotFile(filename);
+void convexHull::appendToFile(const std::string& filename, const std::string& content) {
+    std::ofstream outFile(filename, std::ios::app);
+    if (!outFile.is_open()) {
+        std::cerr << "Unable to open file for appending.\n";
+        return;
+    }
+    outFile << content;
+    outFile.close();
+}
 
-    if (dotFile.is_open()) {
-        dotFile << "graph hexagon {\n";
+void convexHull::saveToDotFile(const std::vector<Point>& points, const std::string& filename) {
+    std::ifstream fileCheck(filename);
+    bool fileIsEmpty = fileCheck.peek() == std::ifstream::traits_type::eof();
+    fileCheck.close();
+
+    if (fileIsEmpty) {
+        std::ofstream dotFile(filename); // Open file normally for writing if it's empty
+
+        if (!dotFile.is_open()) {
+            std::cerr << "Unable to open DOT file!\n";
+            return;
+        }
+
+        dotFile << "graph convexHull {\n";
         dotFile << "  layout=\"neato\";\n\n";
 
         // Write each point as a node in the DOT file with coordinates as labels
         for (size_t i = 0; i < points.size(); ++i) {
             dotFile << "  " << "P" << i << " [pos=\"" << points[i].x << "," << points[i].y << "!\""
-                    << " label=\"" << std::fixed << std::setprecision(2) << "(" << points[i].x << ", " << points[i].y << ")\"";
+                    << " label=\"" << std::fixed << std::setprecision(2) << "(" << points[i].x << ", " << points[i].y
+                    << ")\"";
             dotFile << (i < points.size() - 1 ? "];\n" : "];\n\n");
         }
 
         // Add edges between points
         for (size_t i = 0; i < points.size(); ++i) {
             dotFile << "  P" << i << " -- "
-                    << "P" << ((i + 1) % points.size())<< (i < points.size() - 1 ? ";\n" : ";\n");
+                    << "P" << ((i + 1) % points.size()) << (i < points.size() - 1 ? ";\n" : ";\n");
         }
 
         dotFile << "}\n";
+
         dotFile.close();
-        std::cout << "DOT file '" << filename << "' has been created.\n";
+        std::cout << "DOT file '" << filename << "' has been updated.\n";
     } else {
-        std::cerr << "Unable to create DOT file!\n";
+        removeLastLine(filename); // Remove the last line '}' to prepare for appending
+
+        std::stringstream content;
+
+        for (size_t i = 0; i < points.size(); ++i) {
+            content << "  " << "P" << i + 50 << " [pos=\"" << points[i].x << "," << points[i].y << "!\""
+                    << " label=\"" << std::fixed << std::setprecision(2) << "(" << points[i].x << ", " << points[i].y
+                    << ")\"";
+            content << (i < points.size() - 1 ? "];\n" : "];\n\n");
+        }
+
+        for (size_t i = 0; i < points.size(); ++i) {
+            content << "  P" << i + 50 << " -- "
+                    << "P" << ((i + 1) % points.size()) + 50 << (i < points.size() - 1 ? ";\n" : ";\n");
+        }
+
+        content << "}\n";
+
+        appendToFile(filename, content.str());
+        std::cout << "DOT file '" << filename << "' has been updated.\n";
+    }
+}
+
+void convexHull::removeLastLine(const std::string& filename) {
+    std::ifstream inFile(filename);
+    std::vector<std::string> lines;
+    std::string line;
+
+    // Read lines from the file and store in a vector
+    while (std::getline(inFile, line)) {
+        lines.push_back(line);
+    }
+
+    inFile.close();
+
+    if (!lines.empty()) {
+        // Remove the last line from the vector
+        lines.pop_back();
+
+        // Rewrite the modified content back to the file
+        std::ofstream outFile(filename);
+        for (const std::string& updatedLine : lines) {
+            outFile << updatedLine << "\n";
+        }
+        outFile.close();
+        std::cout << "Last line removed from the file '" << filename << "'.\n";
+    } else {
+        std::cout << "File is empty. Nothing to remove.\n";
     }
 }
 
 
-void convexHull::print(std::vector<Point> convexHull)
+void convexHull::print(std::vector<Point> convexHull) //print out a vector of points
 {
     for(int i = 0; i < convexHull.size();i++)
     {
@@ -141,18 +210,18 @@ void convexHull::print(std::vector<Point> convexHull)
     std::cout << "(" << convexHull[0].x << "," << convexHull[0].y << ")";
 }
 
-int convexHull::orientation(Point p1, Point p2, Point p3) //fix up
+int convexHull::orientation(Point p1, Point p2, Point p3) //takes 3 points to determine their orientation to one another
 {
 
     int val = (p2.x - p1.x) * (p3.y - p2.y) - (p2.y - p1.y) * (p3.x - p2.x);
     if (val == 0)
         return 0; // collinear
 
-    return (val > 0) ? 1 : 2; //
+    return (val > 0) ? 1 : 2; //returns one if a counterclockwise turn is made, 2 if clockwise
 }
 
 
-int convexHull::findLowestY(std::vector<Point> dataSet)
+int convexHull::findLowestY(std::vector<Point> dataSet) //find the lowest Y value in a vector of Points
 {
     int index = 0;
     for(int i = 1; i < dataSet.size(); i++)
@@ -172,12 +241,12 @@ int convexHull::findLowestY(std::vector<Point> dataSet)
     return index;
 }
 
-double convexHull::findAngle(Point anchor, Point p2)
+double convexHull::findAngle(Point anchor, Point p2) //finds the angle between 2 points
 {
     return atan2(p2.y - anchor.y, p2.x - anchor.x);
 }
 
-void convexHull::selection_sort(std::vector<Point> &dataSet)
+void convexHull::selection_sort(std::vector<Point> &dataSet) //sorts a vector of points smallest to largest angle
 {
     int min_indx;
     for(int i = 1; i < dataSet.size(); i++)
@@ -203,14 +272,14 @@ void convexHull::selection_sort(std::vector<Point> &dataSet)
     }
 }
 
-void convexHull::swap(Point &p1, Point &p2)
+void convexHull::swap(Point &p1, Point &p2) //swap two points
 {
     Point temp = p1;
     p1 = p2;
     p2 = temp;
 }
 
-std::vector<Point> convexHull::initialize(std::string file)
+std::vector<Point> convexHull::initialize(std::string file) //creates the initial vector that holds data points from a file
 {
     //read in the file and store each point x and y coordinate into a pair
     //which is then stored into a vector together
