336501489fa17a8499a166b5f9d2a376eeb341f8
zacknotarianni <123593640+zacknotarianni@users.noreply.github.com>
2023-12-04 23:03:46 -0500

    Update mainex2.cpp

--- code/mainex2.cpp
+++ code/mainex2.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
 #include "convexHull.h"
 
 
 int main(int argv, char* argc[]) {
-    
-    convexHull hullObj; // Create an object of the convexHull class
-    std::vector<Point> data = hullObj.createConvexHull(argc[1]); 
-    std::vector<Point> data2 = hullObj.createConvexHull(argc[2]);
 
+    std::ofstream file("output.dot", std::ios::trunc); //clear output.dot
+    convexHull hullObj; // Create an object of the convexHull class
+    std::vector<Point> data = hullObj.createConvexHull("points.txt"); //creates a DOT file and stores the CH in data
+    std::vector<Point> data2 = hullObj.createConvexHull("points2.txt");
 }
